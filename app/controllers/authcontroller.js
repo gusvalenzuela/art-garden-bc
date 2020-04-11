dc78a1e66015bcec1378ea433635178c85a33400
Gus (Nala) <gusrosales7@gmail.com>
2020-04-11 14:07:08 -0700

    route changes

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -17,32 +17,17 @@ exports.profile = function (req, res) {
 		include: [db.Userdetail, db.Artwork, db.Request],
 		raw: false,
 	}).then(response => {
-		console.log(utils.filterUserResponse(response).response)
 		res.render(`profile-mark`, utils.filterUserResponse(response).response)
 	})
 }
 
-// exports.requestform = function (req, res) {
-
-// 	db.User.findOne({
-// 		where: {
-// 			id: req.user.id,
-// 		},
-// 		include: [db.Userdetail, db.Artwork, db.Request],
-// 		raw: false
-// 	}).then(response => {
-// 		res.render(`profile`, response.dataValues)
-// 	})
-
-// }
-
 exports.grvTest = function (req, res) {
-	console.log(req.user)
+	// console.log(req.user)
 	res.render("grv-test", utils.filterUserResponse(req.user).user)
 }
 
 exports.antTest = function (req, res) {
-	console.log(req.user)
+	// console.log(req.user)
 	res.render("ant-test", req.user)
 }
 
