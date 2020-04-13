17674da00267fefeaf8452cacab9848b7e3b54e4
G Val <13578427+gusvalenzuela@users.noreply.github.com>
2020-04-13 15:36:42 -0700

    password cleaner changed

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -1,7 +1,6 @@
 module.exports = {
 	isLoggedIn(req, res, next) {
 		if (req.isAuthenticated()) return next()
-
 		res.redirect("/")
 	},
 
@@ -9,17 +8,14 @@ module.exports = {
 		if (response.dataValues) {
 			// response.dataValues.password = `hunter2`
 			response.dataValues.password = `*`.repeat(7)
-			response.dataValues.fullName =
-				response.firstName + ` ` + response.lastName
+			response.firstName + ` ` + response.lastName
 		} else if (response.password) {
 			response.password = `*`.repeat(response.password.length)
 		}
 
 		return {
 			response: response.dataValues,
-
 			user: response,
-
 			fullName: response.firstName + ` ` + response.lastName,
 		}
 	},
