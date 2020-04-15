b842955402c2ff81bf4cc44c73f4dce6763a85c3
Mark <mso872@gmail.com>
2020-04-14 21:26:19 -0700

    restyled profile

--- app/config/config.js
+++ app/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": "marksql#15",
     "database": "artgarden",
     "host": "localhost",
     "dialect": "mysql",
